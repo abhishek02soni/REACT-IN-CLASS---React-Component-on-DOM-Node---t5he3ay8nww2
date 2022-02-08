e6d2836c612c3356dc3e3971dfdcba860ec0af28
abhishek02soni <92570611+abhishek02soni@users.noreply.github.com>
2022-02-08 13:50:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-  return (
-    <div id="root">
-    <p>I am learning React. My life is getting better.</p>
-    </div>
-  )
+const App =()=>{
+  return( <p></p>);
 }
-
-
 export default App;
-
